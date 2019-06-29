aa73159f59ed7a43cfd0c50060bb65447db3d2ee
neroark <ns_ark@hotmail.com>
2019-06-29 16:06:02 +0200

    feat: add option width

--- bigimage.js
+++ bigimage.js
@@ -1,12 +1,28 @@
 const Jimp = require("jimp");
+const chalk = require("chalk");
+
+const argv = require("yargs")
+  .option("file", {
+    alias: "f",
+    describe: `The name of the file to use`,
+    type: "string",
+    require: true
+  })
+  .option("width", {
+    alias: "w",
+    describe: `Set image width in px, default:1500px`,
+    type: "string"
+  }).argv;
+
+console.log(chalk`{cyan Opening file ${argv.file}}`);
 
 Jimp.read("./in/01.jpg")
   .then(img => {
     return img
-      .resize(256, 256) // resize
-      .quality(60) // set JPEG quality
-      .greyscale() // set greyscale
-      .write("01-bw.jpg"); // save
+      .resize(256, 256)
+      .quality(60)
+      .greyscale()
+      .write("./out/01-bw.jpg");
   })
   .catch(err => {
     console.error(err);
